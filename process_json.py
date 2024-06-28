e26aca326d335c9bb0f94f7cf278042784d91f9a
Zuzana Sebechlebska <69768202+ZuzanaSebb@users.noreply.github.com>
2024-06-28 15:48:14 +0200

    cleaning process_json.py

--- process_json.py
+++ process_json.py
@@ -1,10 +1,8 @@
 import os
 import json
+import re
 
-# Path to the JSON file
 ontology_path = os.getenv('JSON_FILE_PATH')
-
-
 try:
     with open(ontology_path, 'r') as f:
         ontology = json.load(f)
@@ -15,12 +13,7 @@ except json.JSONDecodeError as e:
     print(f"Error decoding JSON: {e}")
     exit(1)
 
-# Get the comment body from environment variables
 ontology_string = os.getenv('ONTOLOGY_STRING')
-print(ontology_string)
-# Define the regex pattern to match the comment format
-import re
-
 comment_regex = r'^([a-zA-Z]+)-([a-zA-Z]+)-([a-zA-Z]+)$'
 match = re.match(comment_regex, ontology_string)
 
@@ -28,7 +21,6 @@ if not match:
     print('Comment format incorrect. Please use \'/approved ontology string:level1-level2-level3\'.')
     exit(1)
 
-# Extract level1, level2, and level3 from the comment
 level1, level2, level3 = match.groups()
 
 # Update the ontology JSON structure
@@ -39,7 +31,6 @@ if level2 not in ontology[level1]:
 if level3 not in ontology[level1][level2]:
     ontology[level1][level2].append(level3)
 
-# Write the updated JSON back to the file
 try:
     with open(ontology_path, 'w') as f:
         json.dump(ontology, f, indent=2)
